83beb529a6cd87afdc322119bed3743cc4f2b334
SeanFuller294 <s.fuller294@gmail.com>
2019-10-29 12:38:12 -0600

    created a 3d array to add rooms

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -15,167 +15,94 @@ namespace ConsoleAdventure.Project.Models
     public void Setup()
     {
       Random random = new Random();
-      string trapString()
+      string roomString(int x)
       {
-        int rand = random.Next(1, 4);
-        string tString = "";
-        if (rand == 1)
+        if (x == 1)
         {
-          tString = "In a flash you see red before being consumed by fiery death";
-        }
-        else if (rand == 2)
-        {
-          tString = "Water floods the room. You try to go back, but the door is locked. You drown.";
-        }
-        else if (rand == 3)
-        {
-          tString = "Blades shoot out from every direction slicing you into pieces!";
+          int rand = random.Next(1, 4);
+          switch (rand)
+          {
+            case 1: return "In a flash you see red before being consumed by fiery death";
+            case 2: return "Water floods the room. You try to go back, but the door is locked. You drown.";
+            case 3: return "Blades shoot out from every direction slicing you into pieces!";
+            case 4: return "Spikes shoot up from the floor impaling you!";
+          }
         }
         else
         {
-          tString = "Spikes shoot up from the floor impaling you!";
+          int rand = random.Next(1, 5);
+          switch (rand)
+          {
+            case 1: return "A strange room with doors on all sides and a ladder in the middle. A dim red light illuminates the room.";
+            case 2: return "A strange room with doors on all sides and a ladder in the middle. A dim orange light illuminates the room.";
+            case 3: return "A strange room with doors on all sides and a ladder in the middle. A dim yellow light illuminates the room.";
+            case 4: return "A strange room with doors on all sides and a ladder in the middle. A dim green light illuminates the room.";
+            case 5: return "A strange room with doors on all sides and a ladder in the middle. A dim blue light illuminates the room.";
+          }
         }
-        return tString;
+        return "";
       }
-      //SECTION Add rooms
-      Room room1 = new Room("room1", trapString(), true);
-      Room room2 = new Room("room2", trapString(), true);
-      Room room3 = new Room("room3", "You scan the room, not seeing anything noteworthy. What you do notice is that it smells faintly of onions.", false);
-      Room room4 = new Room("room4", "A dim green light illuminates the room.", false);
-      Room room5 = new Room("room5", "A strange room with doors on all sides and a ladder in the middle.", false);
-      Room room6 = new Room("room6", trapString(), true);
-      Room room7 = new Room("room7", "A dim blue light illuminates the room.", false);
-      Room room8 = new Room("room8", "An inch of water covers the floor. You don't like it here.", false);
-      Room room9 = new Room("room9", "You hear the distant sound of someone crying. You look around, but the room is empty.", false);
-      Room room10 = new Room("room10", "A soft white light illuminates the room. You feel safe here.", false);
-      Room room11 = new Room("room11", "Carved on the south wall are the words: 'No Hope'", false);
-      Room room12 = new Room("room12", "A room like the rest. This one has padded walls.", false);
-      Room room13 = new Room("room13", "The walls are mirrored, you see your grimy reflection in every surface.", false);
-      Room room14 = new Room("room14", trapString(), true);
-      Room room15 = new Room("room15", "A toaster stands alone in the middle of the room. You feel the need to climb on top of it.", false);
-      Room room16 = new Room("room16", "You look around and see a liquid dripping down the walls. The room smells musty. You don't like it here.", false);
-      Room room17 = new Room("room17", trapString(), true);
-      Room room18 = new Room("room18", "The room is black. The only light streams from one dim light on each door.", false);
-      Room room19 = new Room("room19", "A skeleton sits in the corner. You try to pick up it's bones, but every one you touch crumbles in your fingers.", false);
-      Room room20 = new Room("room20", trapString(), true);
-      Room room21 = new Room("room21", "It's scary.", false);
-      Room room22 = new Room("room22", "A dim yellow light illuminates the room.", false);
-      Room room23 = new Room("room23", "You hear elevator music playing. You look around, but see no speakers.", false);
-      Room room24 = new Room("room24", "There is an image of a cake on the south door.", false);
-      Room room25 = new Room("room25", "A bright red and yellow warning light flashes over and over.", false);
-      Room room26 = new Room("room26", trapString(), true);
-      Room room27 = new Room("room27", trapString(), true);
+      var rooms = new Room[3, 3, 3];
+      int i = 1;
+      var trappedRooms = new HashSet<string>();
+      trappedRooms.Add("0,2,0");
+      trappedRooms.Add("1,2,0");
+      trappedRooms.Add("2,1,0");
+      trappedRooms.Add("1,0,1");
+      trappedRooms.Add("1,1,1");
+      trappedRooms.Add("1,0,2");
+      trappedRooms.Add("1,2,2");
+      trappedRooms.Add("2,0,2");
+      for (int x = 0; x < rooms.GetLength(0); x++)
+      {
+        for (int y = 0; y < rooms.GetLength(1); y++)
+        {
+          for (int z = 0; z < rooms.GetLength(2); z++)
+          {
+            string xyz = $"{x},{y},{z}";
+            if (trappedRooms.Contains(xyz))
+            {
+              rooms[x, y, z] = new Room($"room{i}", roomString(1), true);
+            }
+            else
+            {
+              rooms[x, y, z] = new Room($"room{i}", roomString(2), false);
+            }
+            i++;
+          }
+        }
+      }
+      for (int x = 0; x < rooms.GetLength(0); x++)
+      {
+        for (int y = 0; y < rooms.GetLength(1); y++)
+        {
+          for (int z = 0; z < rooms.GetLength(2); z++)
+          {
+            rooms[x, y, z].AddRooms(x, y, z, rooms);
+          }
+        }
+      }
       Room room28 = new Room("room28", "You have found the exit to the cube, you go through and escape this prison.", false);
-
-      CurrentRoom = room5;
-
-      //!SECTION 
-
-      //SECTION First Floor Connections
-      room3.AddExit("west", room2);
-      room3.AddExit("up", room12);
-      room3.AddExit("south", room6);
-
-      room4.AddExit("north", room1);
-      room4.AddExit("east", room5);
-      room4.AddExit("south", room7);
-      room4.AddExit("up", room13);
-
-      room5.AddExit("north", room2);
-      room5.AddExit("east", room6);
-      room5.AddExit("south", room8);
-      room5.AddExit("west", room4);
-      room5.AddExit("up", room14);
-
-      room7.AddExit("north", room4);
-      room7.AddExit("east", room8);
-      room7.AddExit("up", room16);
-
-      room8.AddExit("north", room5);
-      room8.AddExit("east", room9);
-      room8.AddExit("west", room7);
-      room8.AddExit("up", room17);
-
-      room9.AddExit("north", room6);
-      room9.AddExit("west", room8);
-      room9.AddExit("up", room18);
-      //!SECTION 
-
-      //SECTION Second Floor Connections 
-      room10.AddExit("east", room11);
-      room10.AddExit("south", room13);
-      room10.AddExit("down", room1);
-      room10.AddExit("up", room19);
-
-      room11.AddExit("east", room12);
-      room11.AddExit("south", room14);
-      room11.AddExit("west", room10);
-      room11.AddExit("down", room2);
-      room11.AddExit("up", room20);
-
-      room12.AddExit("south", room15);
-      room12.AddExit("west", room11);
-      room12.AddExit("down", room3);
-      room12.AddExit("up", room21);
-
-      room13.AddExit("north", room10);
-      room13.AddExit("east", room14);
-      room13.AddExit("south", room16);
-      room13.AddExit("down", room4);
-      room13.AddExit("up", room22);
-
-      room15.AddExit("north", room12);
-      room15.AddExit("south", room18);
-      room15.AddExit("west", room14);
-      room15.AddExit("down", room6);
-      room15.AddExit("up", room24);
-
-      room16.AddExit("north", room13);
-      room16.AddExit("east", room17);
-      room16.AddExit("down", room7);
-      room16.AddExit("up", room25);
-
-      room18.AddExit("north", room15);
-      room18.AddExit("west", room17);
-      room18.AddExit("down", room9);
-      room18.AddExit("up", room27);
-
-      //!SECTION 
-
-      //SECTION Third Floor Connections
-
-      room19.AddExit("east", room20);
-      room19.AddExit("south", room22);
-      room19.AddExit("down", room10);
-
-      room21.AddExit("south", room24);
-      room21.AddExit("east", room20);
-      room21.AddExit("down", room12);
-
-      room22.AddExit("north", room19);
-      room22.AddExit("east", room23);
-      room22.AddExit("south", room25);
-      room22.AddExit("down", room13);
-
-      room23.AddExit("north", room20);
-      room23.AddExit("east", room24);
-      room23.AddExit("south", room26);
-      room23.AddExit("west", room22);
-      room23.AddExit("down", room14);
-
-      room24.AddExit("north", room21);
-      room24.AddExit("south", room27);
-      room24.AddExit("west", room23);
-      room24.AddExit("east", room28);
-      room24.AddExit("down", room15);
-
-      room25.AddExit("north", room22);
-      room25.AddExit("east", room26);
-      room25.AddExit("down", room16);
-
-      //!SECTION
-
-
+      rooms[2, 1, 2].AddExit("east", room28);
+      CurrentRoom = rooms[1, 1, 0];
     }
   }
 }
+
+// case 1: return "You scan the room, not seeing anything noteworthy. What you do notice is that it smells faintly of onions.";
+// case 2: return "A dim green light illuminates the room.";
+// case 3: return "A bright red and yellow warning light flashes over and over.";
+// case 4: return "A dim blue light illuminates the room.";
+// case 5: return "An inch of water covers the floor. You don't like it here.";
+// case 6: return "You hear the distant sound of someone crying. You look around, but the room is empty.";
+// case 7: return "A soft white light illuminates the room. You feel safe here.";
+// case 8: return "Carved on the south wall are the words: 'No Hope'";
+// case 9: return "A room like the rest. This one has padded walls.";
+// case 10: return "The walls are mirrored, you see your grimy reflection in every surface.";
+// case 11: return "A toaster stands alone in the middle of the room. You feel the need to climb on top of it.";
+// case 12: return "You look around and see a liquid dripping down the walls. The room smells musty. You don't like it here.";
+// case 13: return "The room is black. The only light streams from one dim light on each door.";
+// case 14: return "A skeleton sits in the corner. You try to pick up it's bones, but every one you touch crumbles in your fingers.";
+// case 15: return "It's scary.";
+// case 16: return "A dim yellow light illuminates the room.";
+// case 17: return "You hear elevator music playing. You look around, but see no speakers.";
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -45,5 +45,33 @@ namespace ConsoleAdventure.Project.Models
       Exits.Add(direction, room);
     }
 
+    public void AddRooms(int x, int y, int z, Room[,,] rooms)
+    {
+      if (x > 0)
+      {
+        AddExit("west", rooms[x - 1, y, z]);
+      }
+      if (x < rooms.GetLength(0) - 1)
+      {
+        AddExit("east", rooms[x + 1, y, z]);
+      }
+      if (y > 0)
+      {
+        AddExit("south", rooms[x, y - 1, z]);
+      }
+      if (y < rooms.GetLength(1) - 1)
+      {
+        AddExit("north", rooms[x, y + 1, z]);
+      }
+      if (z > 0)
+      {
+        AddExit("down", rooms[x, y, z - 1]);
+      }
+      if (z < rooms.GetLength(2) - 1)
+      {
+        AddExit("up", rooms[x, y, z + 1]);
+      }
+    }
+
   }
 }
